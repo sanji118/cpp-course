42abbd3dd74d69ff0d4bed21e277b42a62548055
sanji118 <sanjishifa32@gmail.com>
2024-09-26 15:15:47 +0600

    added 2 cpp files

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!! ";
+    std::cout << "\nWelcome to cpp world!!" << endl;
+    cout << "I am learning C++";
+    return 0;
+}
+
---
+++ variable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<conio.h>
+#include <string>
+using namespace std;
+
+int main(){
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 5.99898764445;
+    char myLetter = 'd';
+    string myString = "Hello";
+    bool myBoolean = true;
+
+    //print variables 
+
+    cout << "int: " << myNum << "\n";
+    cout << "float: " << myFloatNum << "\n";
+    cout << "double: " << myDoubleNum << "\n";
+    cout << "char: " << myLetter << "\n";
+    cout << "bool: " << myBoolean << "\n";
+    cout << "string: " << myString << "\n";
+}
