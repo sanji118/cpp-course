145965f54e29287fc3a94dde8ca2bb5748b8fc9f
sanji118 <sanjishifa32@gmail.com>
2024-10-09 12:57:44 +0600

    added a new files

--- strconcate.cpp
+++ strconcate.cpp
@@ -7,5 +7,22 @@ int main(){
     string firstName = "John";
     string lastName = "Doe";
     string fullName = firstName + " "+ lastName;
-    cout << fullName;
+    cout << fullName << endl;
+    string fullName2 = firstName.append(lastName);
+    cout << fullName2 << endl;
+    string txt = "Hello World!";
+    cout << txt.at(0);//string.at(index);
+    cout << txt.at(txt.length() - 1) << endl;
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length() << endl;//string.length();
+    return 0;
 }
+
+//The + operator can be used between strings to add them together to make a new string. This is called concatenation
+
+/**1.find()	Returns the index (position) of the first occurrence of a string or character
+2.rfind()	Returns the index (position) of the last occurrence of a string or character
+3.replace()	Replaces a part of a string with another string
+4.insert()	Inserts a string at a specified index (position)
+5.erase()	Removes characters from a string
+6.compare()	Compares two strings*/
